8010f815ef5809d065486fb1dc380ecbf5beef8a
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-03-09 13:23:03 +0530

    sync

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,18 @@
-function About() {
-    return <div>About</div>
+const About = ({ buildTimestamp }) => {
+  return (
+    <p>
+      Hello From my next.js app! Testing this 
+      App built at: {buildTimestamp}
+    </p>
+  )
+}
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      buildTimestamp: Date.now()
+    }
   }
-  
-  export default About
+}
+
+export default About;
