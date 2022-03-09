3e0bbed0c18826e40ca5b11ba209b0b3b918fcc4
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-03-09 12:55:54 +0530

    first commit

---
+++ utils/shopify.js
@@ -0,0 +1,5 @@
+import Client from "shopify-buy";
+export const client = Client.buildClient({
+  storefrontAccessToken: "2637c8a86cce005b7d19baf8a8760fdb",
+  domain: "sheetal1993.myshopify.com",
+});
