6e28a55c99db7f6ea6be2bc77e3bae589f43cf7f
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-03-23 13:02:23 +0530

    added airtable function

---
+++ functions/airtable.js
@@ -0,0 +1,26 @@
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE_ID)
+  .table(process.env.AIRTABLE_TABLE_NAME)
+exports.handler = async (event,context) => {
+    try {
+        const {records} = airtable.list();
+        const products = records.map((product) => {
+            const {id} = product;
+            const {name,unit,price,desc,type} = product.fields;
+            return {id,name,unit,type,desc,price}
+        })
+        return {
+            statusCode: 200,
+            body: JSON.stringify(products),
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'server error',
+        } 
+    }
+    
+    
+}
