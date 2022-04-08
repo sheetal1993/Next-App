2dc4e87f045f1bcefff27dbf2dc4f8590721b954
Sheetal Raikar <sheetal_raikar@persistent.com>
2022-04-08 09:56:10 +0530

    added algolia search

---
+++ pages/search.js
@@ -0,0 +1,25 @@
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits }
+    from "react-instantsearch-dom";
+
+    const searchClient = algoliasearch(
+      'VN3BCP1RCW',
+      'e4190f9c2f41083cf64ec16c723fbe5b',
+    );
+    
+export default function SearchBar() {
+    return (
+      <>
+        <InstantSearch 
+          searchClient={searchClient} 
+          indexName="shopify_devpocproducts">
+    
+          {/* Adding Search Box */}
+          <SearchBox />
+    
+          {/* Adding Data */}
+          <Hits />
+        </InstantSearch>
+      </>
+    );
+  }
